3bd3b26988a5bc01afe0d65efdb5623f3288e7fd
Tntbaba <baptiste.dallara4@gmail.com>
2023-05-14 10:21:49 +0200

    [+]AutorResearch skeleton

--- Projet_java/src/Controller/AuthorDisplayController.java
+++ Projet_java/src/Controller/AuthorDisplayController.java
@@ -1,60 +1,55 @@
 package Controller;
 
-import java.net.URL;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.ResourceBundle;
 import java.sql.ResultSet;
 
 import DataAccess.SingletonConnexion;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import Model.*;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
 
 public class AuthorDisplayController {
 
     @FXML
-    private TableView<Contributor> authorTable;
+    private TextField authorFName;
 
     @FXML
-    private Button btnSearchAuthor;
+    private TextField authorLName;
 
     @FXML
-    private TextField inputAuthor;
+    private TableColumn<?, ?> restultSearch;
 
     @FXML
-    private TableColumn<Contributor, String> columnAuthorFName;
+    private TableColumn<?, ?> tableName;
 
     @FXML
-    private TableColumn<Contributor, String> columnAuthorLName;
-
-    public ArrayList<Contributor> authors = new ArrayList<>();
-
-    @FXML
-    public void onSearchButtonClicked() throws SQLException {
+    void onButtonSearchClicked(ActionEvent event) {
         try {
             Connection connection = SingletonConnexion.getUniqueConnexion();
-            String sql = "SELECT * FROM person WHERE personType = 'Author' AND lastName = ?";
+            String sql = "SELECT * FROM person WHERE personType = 'Author' AND lastName = ? AND firstName = ?";
             PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setString(1, inputAuthor.getText());
-            System.out.println("hello");
+            statement.setString(1, authorLName.getText());
+            statement.setString(2, authorFName.getText());
             ResultSet data = statement.executeQuery();
             while (data.next()) {
-                Contributor author = new Contributor(data.getInt("personId"), data.getString("firstName"), data.getString("lastName"));
-                System.out.println(author.getFirstName());
-                authors.add(author);
+
             }
-            columnAuthorFName.setCellValueFactory(new PropertyValueFactory<>("firstName"));
-            columnAuthorLName.setCellValueFactory(new PropertyValueFactory<>("lastName"));
-            authorTable.getItems().setAll(authors);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
+
+    @FXML
+    void onComboBoxClicked(ActionEvent event) {
+
+    }
+
 }
