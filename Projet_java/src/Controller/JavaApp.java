19a7c0b531bfddc96043ab61a86486e178706871
blackpepere <etu48636@henallux.be>
2023-05-16 10:54:28 +0200

    [*] Fix All View Position

--- Projet_java/src/Controller/JavaApp.java
+++ Projet_java/src/Controller/JavaApp.java
@@ -13,7 +13,7 @@ public class JavaApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(JavaApp.class.getResource("../View/MainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 700, 550);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("My Library");
         Image logo = new Image("View/icon/livre.png");
         stage.getIcons().add(logo);
